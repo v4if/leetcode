cdbce044fdd04225d6ce2d69f339ae9bd893b1eb
atkjest <atkjest@gmail.com>
2017-04-23 16:09:48 +0800

    2017-04-23 16:09:48

--- gtest/tests/015_3sum.cpp
+++ gtest/tests/015_3sum.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <set>
 #include <gtest/gtest.h>
 
 /*
@@ -24,12 +26,59 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<int> visited;
+        sort(nums.begin(), nums.end());
 
+//        遍历，然后双向指针取target值，重复值去掉
+        for (int i = 0; i < nums.size(); ++i) {
+            if (find(visited.begin(), visited.end(), nums[i]) != visited.end()) {
+                continue;
+            }
+
+            visited.insert(nums[i]);
+
+            int target = -nums[i];
+            int front = i + 1;
+            int end = nums.size() - 1;
+
+            while (front < end) {
+                int sums = nums[front] + nums[end];
+
+                if (sums < target) {
+                    front++;
+                } else if (sums > target) {
+                    end--;
+                } else {
+                    //vector<T> v(n,i)形式，v包含n 个值为 i 的元素
+                    vector<int> triplet(3, 0);
+                    triplet[0] = nums[i];
+                    triplet[1] = nums[front];
+                    triplet[2] = nums[end];
+                    ans.push_back(triplet);
+
+//                    处理相同元素
+                    while (front < end && nums[front] == triplet[0]) front++;
+                    while (front < end && nums[end] == triplet[2]) end--;
+                }
+            }
+        }
+
+        return ans;
     }
 };
 
 TEST(leetcode, _3sum) {
-    int myints[] = {32,71,12,45,26,80,53,33};
-    std::vector<int> myvector (myints, myints+8);
+    int myints[] = {-1, 0, 1, 2, -1, -4};
+    std::vector<int> myvector (myints, myints + 6);
 
+    vector<vector<int>> result;
+    result = Solution().threeSum(myvector);
+
+    for (int i = 0; i < result.size(); ++i) {
+        for (int j = 0; j < result[i].size(); ++j) {
+            cout << result[i][j] << "  " << endl;
+        }
+        cout << endl;
+    }
 }

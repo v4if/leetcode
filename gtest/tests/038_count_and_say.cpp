ce32e0647129cdf828274a03be8f0b3fa6933dde
atkjest <atkjest@gmail.com>
2017-05-20 15:41:53 +0800

    2017-05-20 15:41:53

---
+++ gtest/tests/038_count_and_say.cpp
@@ -0,0 +1,43 @@
+//
+// Created by root on 5/20/17.
+//
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+/*
+    The count-and-say sequence is the sequence of integers beginning as follows:
+    1, 11, 21, 1211, 111221, ...
+
+    1 is read off as "one 1" or 11.
+    11 is read off as "two 1s" or 21.
+    21 is read off as "one 2, then one 1" or 1211.
+    Given an integer n, generate the nth sequence.
+ * */
+
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 0) return "";
+        string res = "1";
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.size(); i++) {
+                int count = 1;
+                while ((i + 1 < res.size()) && (res[i] == res[i + 1])){
+                    count++;
+                    i++;
+                }
+                cur += to_string(count) + res[i];
+            }
+            res = cur;
+        }
+        return res;
+    }
+};
+
+TEST(leetcode, count_and_say) {
+    EXPECT_EQ("1211", Solution().countAndSay(4));
+}

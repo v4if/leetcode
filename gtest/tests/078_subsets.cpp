8fa8ab9dd6e4ea49780937d4cdbc002dec94c9ad
v4if <atkjest@gmail.com>
2017-07-02 16:48:05 +0800

    2017/07/02 16:47:49

--- gtest/tests/078_subsets.cpp
+++ gtest/tests/078_subsets.cpp
@@ -46,10 +46,25 @@ namespace solve_first {
     }
 }
 
+namespace solve_best{
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> subs(1, vector<int>());
+        for (int i = 0; i < nums.size(); i++) {
+            int n = subs.size();
+            for (int j = 0; j < n; j++) {
+                subs.push_back(subs[j]);
+                subs.back().push_back(nums[i]);
+            }
+        }
+        return subs;
+    }
+}
+
 TEST(leetcode, subsets_test) {
     vector<vector<int>> ans;
     vector<int> nums{1,2};
-    ans = solve_first::subsets(nums);
+    ans = solve_best::subsets(nums);
     for (int i = 0; i < ans.size(); ++i) {
         for (int j = 0; j < ans[i].size(); ++j) {
             cout << ans[i][j] << " ";

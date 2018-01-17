54d5a8c0e9e57456a03bc459abe73fd96284bc64
Jichao Wu <karma_wjc@yeah.net>
2018-01-17 13:01:44 +0800

    Update 001_two_sum.cc

--- topInterview/001_two_sum.cc
+++ topInterview/001_two_sum.cc
@@ -1,1 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> twosum(vector<int>& nums, int target) {
+    map<int, int> m;
+    int sz = nums.size();
+    for (int i = 0; i < sz; i++) {
+        int remain = target - nums[i];
+        auto iter = m.find(remain);
+        if (iter != m.end()) {
+            return vector<int>{iter->second, i};
+        } else {
+            m[nums[i]] = i;
+        }
+    }
+    
+    return vector<int>{};
+}
+
+int main() {
+    vector<int> nums{1,2,3,4};
+    vector<int> result = twosum(nums, 5);
+    for (int r : result) {
+        cout << r << " ";
+    }
+    cout << "\n";
+}

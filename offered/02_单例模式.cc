db777e9cafe9b4104acc482f627e3d52d16d47dc
Jichao Wu <karma_wjc@yeah.net>
2018-01-29 17:08:08 +0800

    Create 02_单例模式.cc

---
+++ offered/02_单例模式.cc
@@ -0,0 +1,48 @@
+class Singleton{
+public:
+    Singleton* getInstance() {
+        if (m_pData == NULL) {
+            m_pData = new Singleton();
+        }
+        return m_pData;
+    }
+private:
+    Singleton();
+    static Singleton* m_pData;
+};
+
+Singleton* Singleton::m_pData = NULL;
+
+//==================================
+template<typename T>  
+class Singleton : boost::noncopyable  
+{
+    public:
+        static T& instance()
+        {
+            pthread_once(&ponce_, &Singleton::init);
+            return *value_;
+        }
+
+        static void init()
+        {
+            value_ = new T();
+        }
+    private:
+        static pthread_once_t ponce_;
+        static T* value_;
+
+/*
+private:
+    Singleton();                            // ctor hidden
+    Singleton(Singleton const&);            // copy ctor hidden
+    Singleton& operator=(Singleton const&); // assign op. hidden
+    ~Singleton();                           // dtor hidden
+ */
+};
+
+template<typename T>  
+pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
+
+template<typename T>  
+T* Singleton<T>::value_ = NULL;  

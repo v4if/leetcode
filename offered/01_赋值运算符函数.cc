9e3d64a95c7d49d7e6d4395111cc5553cbede894
Jichao Wu <karma_wjc@yeah.net>
2018-01-29 16:55:50 +0800

    Create 01_赋值运算符函数.cc

---
+++ offered/01_赋值运算符函数.cc
@@ -0,0 +1,29 @@
+/**
+class MyStr{
+public:
+    MyStr(char* pData = NULL):m_pData(pData) {}
+    ~MyStr() {delete m_pData;}
+private:
+    char* m_pData;
+};
+ */
+ 
+MyStr& MyStr::operator= (const MyStr& other) {
+    if (this == &other) return *this;
+
+    delete[] m_pData;
+    m_pData = NULL;
+    m_pData = new char[strlen(other.m_pData) + 1];
+    return *this;
+}
+
+MyStr& Mystr::operator= (const Mystr& other) {
+    if (this != &other) {
+        MyStr strTemp(other.m_pData);
+
+        char* pTemp = strTemp.m_pData;
+        strTemp.m_pData = m_pData;
+        m_pData = pTemp;
+    }
+    return *this;
+}

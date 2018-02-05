803f91d326e4ca7b8bdebe62e1a157947549607d
Jichao Wu <karma_wjc@yeah.net>
2018-02-05 11:32:46 +0800

    Update 组合数.cc

--- topInterview/组合数.cc
+++ topInterview/组合数.cc
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -12,6 +11,40 @@ void dfs(int n, int cur, vector<int>& result) {
         dfs(n, i + 1, result);
         
         result.pop_back();
+    }
+}
+
+void solveit(int n) {
+    stack<int> st;
+    vector<int> visit;
+    vector<int> result;
+    st.push(1);
+    visit.push_back(1);
+    result.push_back(1);
+    while (!st.empty()) {
+        int cur = st.top();
+        bool hasNext = false;
+        while(cur < n) {
+            cur++;
+            if (find(visit.begin(),visit.end(),cur)==visit.end()) {
+                st.push(cur);
+                visit.push_back(cur);
+                result.push_back(cur);
+                hasNext = true;
+                break;
+            }
+        }
+        if (!hasNext) {
+            cur = st.top();
+            for (;cur < n; cur++) {
+                visit.pop_back();
+            }
+            st.pop();
+            
+            for (auto data : result) cout << data << " ";
+            cout << "\n";
+            result.pop_back();
+        }
     }
 }
 

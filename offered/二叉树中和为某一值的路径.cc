11361fd4b7e2481215141dc64bbabf8bbadf9c3e
Jichao Wu <karma_wjc@yeah.net>
2018-03-11 17:36:02 +0800

    Create 二叉树中和为某一值的路径.cc

---
+++ offered/二叉树中和为某一值的路径.cc
@@ -0,0 +1,36 @@
+/*
+输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+*/
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void bt(TreeNode* root, int expect, vector<int>& result, vector<vector<int>>& arr){
+        expect -= root->val;
+        result.push_back(root->val);
+        
+        if (root->left == NULL && root->right == NULL) {
+            if (expect == 0) arr.push_back(result);
+        }
+        
+        if (root->left) bt(root->left, expect, result, arr);
+        if (root->right) bt(root->right, expect, result, arr);
+        result.pop_back();
+    }
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+        if (root == NULL) return vector<vector<int>>{};
+        
+        vector<vector<int>> arr;
+        vector<int> result;
+        bt(root, expectNumber, result, arr);
+        return arr;
+    }
+};

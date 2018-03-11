63c639cf9ad461969457181c24fe24788d707893
Jichao Wu <karma_wjc@yeah.net>
2018-03-11 18:24:23 +0800

    Create 整数中1出现的次数.cc

---
+++ offered/整数中1出现的次数.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+
+    int NumberOf1Between1AndN_Solution(int n){ 
+        int ones = 0;
+        for (long long m = 1; m <= n; m *= 10)
+            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1) * (n%m + 1);
+         return ones;
+     
+    }
+};
